8dfba4c79b55868a7a6dfeeb5325a99d1f9bf68e
Rushdha <rushdha2021@gmail.com>
2023-11-16 21:25:19 +0530

    first commit

---
+++ src/test/java/com/example/myfirstspringbootproject/MyFirstSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
