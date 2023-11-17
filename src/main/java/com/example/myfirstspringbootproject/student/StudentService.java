dc44ed7e4301371500a864ea8d1b4f5c4a01332d
Rushdha <rushdha2021@gmail.com>
2023-11-17 19:38:52 +0530

    added dependency injection

--- src/main/java/com/example/myfirstspringbootproject/student/StudentService.java
+++ src/main/java/com/example/myfirstspringbootproject/student/StudentService.java
@@ -1,9 +1,14 @@
 package com.example.myfirstspringbootproject.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+//@Component
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(
